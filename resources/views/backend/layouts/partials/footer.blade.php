2e931e763fd52ec09e5c8a6e391f451298040805
md_noor <noor.dev@duskbyte.com>
2025-03-05 12:28:19 +0530

    First commit

--- resources/views/backend/layouts/partials/footer.blade.php
+++ resources/views/backend/layouts/partials/footer.blade.php
@@ -3,13 +3,13 @@
         <p>
             © Copyright {{ date("Y") }}. All right reserved. Laravel -
             {{ Illuminate\Foundation\Application::VERSION }} (PHP -
-            {{ PHP_VERSION }}) | Developed by Maniruzzaman Akash
-            <a href="mailto:manirujjamanakash@gmail.com" target="_blank" class="ml-2 mr-2" title="Email Me">
+            {{ PHP_VERSION }}) | Developed by Nuruddin Shah
+            <a href="mailto:shahnuruddin6@gmail.com" target="_blank" class="ml-2 mr-2" title="Email Me">
                 <i class="fa fa-envelope"></i>
             </a>
-            <a href="https://github.com/ManiruzzamanAkash" target="_blank" title="GitHub Profile">
+            <!-- <a href="https://github.com/nurudd" target="_blank" title="GitHub Profile">
                 <i class="fa fa-github"></i>
-            </a>
+            </a> -->
         </p>
     </div>
 </footer>
